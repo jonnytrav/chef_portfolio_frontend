5c314b0e8ff1586dc9e32da501d419a0626f20ee
fritzgt <tuanis@live.com>
2019-09-16 18:16:05 +0300

    Fix navbar to displaydinamically

--- chef-portfolio/src/components/Nav.js
+++ chef-portfolio/src/components/Nav.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 //accessing state for redender myrecipes navlink
-// import useGlobal from '../store';
+import useGlobal from '../store';
 
 function Nav() {
-  // const [globalState, globalActions] = useGlobal();
-  // const { isLoggedIn } = globalState;
+  const [globalState, globalActions] = useGlobal();
+  const { isLoggedIn } = globalState;
   return (
     <div className="nav-bar">
       <NavLink exact className="App-link" activeClassName="active-nav" to="/">
@@ -15,20 +15,42 @@ function Nav() {
       <NavLink className="App-link" activeClassName="active-nav" to="/recipes">
         All Recipes
       </NavLink>
-      <NavLink className="App-link" activeClassName="active-nav" to="/register">
-        Register
-      </NavLink>
-      <NavLink className="App-link" activeClassName="active-nav" to="/login">
-        Login
-      </NavLink>
-      <NavLink
-        className="App-link"
-        activeClassName="active-nav"
-        to="/myrecipes"
-      >
-        My Recipes
-        {/* {isLoggedIn === true && 'My Recipes'} */}
-      </NavLink>
+
+      {isLoggedIn !== true && (
+        <NavLink
+          className="App-link"
+          activeClassName="active-nav"
+          to="/register"
+        >
+          Register
+        </NavLink>
+      )}
+
+      {isLoggedIn !== true && (
+        <NavLink className="App-link" activeClassName="active-nav" to="/login">
+          Login
+        </NavLink>
+      )}
+
+      {isLoggedIn === true && (
+        <NavLink
+          className="App-link"
+          activeClassName="active-nav"
+          to="/myrecipes"
+        >
+          Logout
+        </NavLink>
+      )}
+
+      {isLoggedIn === true && (
+        <NavLink
+          className="App-link"
+          activeClassName="active-nav"
+          to="/myrecipes"
+        >
+          My Recipes
+        </NavLink>
+      )}
     </div>
   );
 }
--- chef-portfolio/src/components/Recipes.js
+++ chef-portfolio/src/components/Recipes.js
@@ -6,7 +6,7 @@ import useGlobal from '../store';
 
 const mapRecipes = recipes => {
   return recipes.map(repo => (
-    <div key={repo.id}>
+    <div key={repo.id} className="container list">
       <h3>{repo.title}</h3>
       <p>{repo.meal_type}</p>
       <p>{repo.ingredients}</p>
@@ -23,12 +23,12 @@ const Recipes = () => {
   const [globalState, globalActions] = useGlobal();
   const { status, recipes } = globalState;
   return (
-    <div className="container">
+    <div>
       {status === 'LOADING' && <h4>Loading...</h4>}
       {status === 'SUCCESS' && mapRecipes(recipes)}
       {status === 'EMPTY' && <h4>You have zero posts</h4>}
       {status === 'NOT_FOUND' && <h4>404 - User not found</h4>}
-      {status === 'ERROR' && <h4>Unauthorized</h4>}
+      {status === 'ERROR' && <h4 className="container list">Unauthorized</h4>}
     </div>
   );
 };
