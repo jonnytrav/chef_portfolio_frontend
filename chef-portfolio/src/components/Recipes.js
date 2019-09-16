ed67838c76728c2c82316863cde6041700ce8193
fritzgt <tuanis@live.com>
2019-09-16 08:25:34 +0300

    completed display recipes list while checking token in headers

--- chef-portfolio/src/components/Recipes.js
+++ chef-portfolio/src/components/Recipes.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function Recipes() {
+import useGlobal from '../store';
+
+const mapRecipes = recipes => {
+  return recipes.map(repo => (
+    <div key={repo.id}>
+      <h3>{repo.title}</h3>
+    </div>
+  ));
+};
+
+const Recipes = () => {
+  //similar to componentDidMount
+  useEffect(() => {
+    globalActions.getRecipes();
+  }, []);
+
+  const [globalState, globalActions] = useGlobal();
+  const { status, recipes } = globalState;
   return (
-    <div>
-      <h1>Recipes List</h1>
+    <div className="container">
+      {status === 'LOADING' && <h4>Loading...</h4>}
+      {status === 'SUCCESS' && mapRecipes(recipes)}
+      {status === 'EMPTY' && <h4>You have zero posts</h4>}
+      {status === 'NOT_FOUND' && <h4>404 - User not found</h4>}
+      {status === 'ERROR' && <h4>Unauthorized</h4>}
     </div>
   );
-}
+};
+
 export default Recipes;
--- chef-portfolio/src/components/axiosAuth.js
+++ chef-portfolio/src/components/axiosAuth.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const axiosWithAuth = () => {
-  const token = localStorage.getItem('userToken');
-
+  const token = localStorage.getItem('authorization');
+  console.log('auth', token);
   return axios.create({
     headers: {
       'Content-Type': 'application/json',
